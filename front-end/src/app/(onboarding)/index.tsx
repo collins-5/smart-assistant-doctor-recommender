a62980cae1af7a7a98c2c12bd887cedc60f2d7d2
collins-5 <otiecollins132@gmail.com>
2026-03-11 14:43:13 +0300

    aded gemini svg

--- front-end/src/app/(onboarding)/index.tsx
+++ front-end/src/app/(onboarding)/index.tsx
@@ -1,12 +1,13 @@
 import { Redirect } from "expo-router";
 import { View, Image } from "react-native";
+import { Image as ExpoImage } from "expo-image";
 import { Text } from "~/components/ui/text";
 import { useSessionInit } from "~/components/core/session-initializer";
 
 export default function OnboardingIndex() {
   const { loading } = useSessionInit();
 
-  if (loading) {
+  if (!loading) {
     return (
       <View className="flex-1">
         <Image
@@ -14,7 +15,7 @@ export default function OnboardingIndex() {
           resizeMode="cover"
           source={require("assets/splash-icon.png")}
         />
-        <View className="absolute inset-0 items-center justify-center">
+        <View className="absolute top-0 left-0 right-0 h-1/2 items-center justify-center">
           <Image
             source={require("assets/sdr-logo.jpg")}
             className="w-24 h-24 rounded-2xl border-2 border-white/30"
@@ -28,8 +29,18 @@ export default function OnboardingIndex() {
           </Text>
         </View>
 
-        <View className="absolute bottom-5 self-center">
-          
+        <View className="absolute bottom-5 self-center flex-row items-center gap-2">
+          <Text className="font-bold text-base italic text-primary-foreground">
+            powered by
+          </Text>
+          <ExpoImage
+            source={require("assets/gemini-color.svg")}
+            style={{ width: 20, height: 20 }}
+            contentFit="contain"
+          />
+          <Text className="font-bold text-base italic text-primary-foreground">
+            Gemini
+          </Text>
         </View>
       </View>
     );
